8e2ae81dd29b0c1688f5b8cba8c60c24b65558fb
Ben-MR <b.rieck@web.de>
2025-03-13 23:05:34 +0100

    first scripts

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let pokemon = [];
-const BASE_URL = "https://pokeapi.co/api/v2/pokemon";
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
 
 function init() {
     fetchPokeData();
@@ -7,17 +7,43 @@ function init() {
 }
 
 async function fetchPokeData() {
-    let pokecard = document.getElementById("pokeContainer"); 
+    
     let response = await fetch(BASE_URL);
     let responseAsJson = await response.json();
-    console.log(responseAsJson.results);
+    console.log(responseAsJson);
     for (let index = 0; index < responseAsJson.results.length; index++) {
-        pokecard.innerHTML += showPokeCard(responseAsJson.results[index], index);
-    }
-    
+       await fetchPokeDetailData(index);
+       
+    }    
 }
 
+async function fetchPokeDetailData(index) {  
+    let pokecard = document.getElementById("pokeContainer"); 
+    pokecardDetail = await fetch(BASE_URL + (index + 1)) ;
+    pokecardDetailAsJson = await pokecardDetail.json();
+    pokecard.innerHTML += showPokeCard(pokecardDetailAsJson);  
+    await new Promise(resolve => setTimeout(resolve, 100));  
+    await fetchType(index);  
+ } 
+    
 
+ async function fetchType(index) {
+    let typeContainer = document.getElementById(`typeContainer-${index}`);
+
+    if (!typeContainer) {
+        console.error(`❌ Fehler: Element typeContainer-${index} existiert nicht!`);
+        return;  // Funktion beenden, wenn das Element nicht existiert
+    }
+
+    let response = await fetch(BASE_URL + (index + 1));
+    let pokeData = await response.json();
+
+    console.log('Typenanzahl für Pokémon ' + (index + 1) + ': ' + pokeData.types.length);
+
+    typeContainer.innerHTML = pokeData.types.map(typeInfo => 
+        `<span>${typeInfo.type.name}</span>`
+    ).join(", ");
+}
 
 
 function formatName(inputName) {
--- script/template.js
+++ script/template.js
@@ -3,16 +3,16 @@ function showPokeCard(pokemon, index) {
             <div id="card-container">    
                 <div class="card-container">
                     <div class="number-name">
-                        <span class="poke-number" id="pokeNumber">#${index + 1}</span>
+                        <span class="poke-number" id="pokeNumber">#${pokemon.id}</span>
                         <span class="poke-name" id="pokeName">${formatName(pokemon.name)}</span>
                         <div></div>
                     </div>
                     <div class="container-poke-image">
-                        <img id="pokeImage" class="poke-image" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="">
+                        <img id="pokeImage" class="poke-image" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg" alt="">
                     </div>
-                    <div class="typ-container">
-                        <img class="image-typ" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/types/generation-viii/brilliant-diamond-and-shining-pearl/12.png" alt="">
-                        <img class="image-typ" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/types/generation-viii/brilliant-diamond-and-shining-pearl/12.png" alt="">
+                    <div id="typeContainer-${index}" class="type-container">
+                        
+                       
                     </div>
                 </div>
             </div>  
