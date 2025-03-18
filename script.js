0982c801dd5fe9ad5d1f897cbffb46a965548e99
Ben-MR <b.rieck@web.de>
2025-03-18 14:53:56 +0100

    overlay general, stats and next, previosu-buttons

--- script.js
+++ script.js
@@ -21,13 +21,13 @@ async function init() {
 }
 
 
-
 async function fetchPokeDataSpecies() {    
     let responseSpecies = await fetch(`${BASE_URL}?limit=${limit}&offset=${offset}`);
     let responseAsJsonSpecies = await responseSpecies.json();        
     for (let index = offset; index < responseAsJsonSpecies.results.length + offset; index++) {
         await renderPokedex(index);
-        await renderGeneralStats(index);
+        await renderOverlayGeneral (index);
+        await renderOverlayStats(index);
     }
 }
 
@@ -51,29 +51,77 @@ async function renderPokedex(index) {
         }, 500);    
 }     
 
-async function renderGeneralStats(index) {
-    let pokecard = document.getElementById("overlayStats");     
+async function renderOverlayGeneral(index) {       
     let response = await fetch(BASE_URL_SPECIES + (offset + index - offset + 1)); 
     let pokeData = await response.json();
     let response2 = await fetch(BASE_URL + (offset + index - offset + 1)); 
-    let pokeData2 = await response2.json();
-    let test = {hp: 30}
-    Object.assign(pokemon[index], test)
-    
-        // pokecard.innerHTML = showOverlayGeneralStats(pokeData, index);
-         
-              
+    let pokeData2 = await response2.json();   
+    let general = {
+        egg1: pokeData.egg_groups[0].name,
+        egg2: pokeData.egg_groups.length > 1 ? pokeData.egg_groups[1].name :null,
+        height: pokeData2.height,
+        weight: pokeData2.weight,
+        ability1: pokeData2.abilities[0].ability.name,
+        ability2: pokeData2.abilities.length >1 ? pokeData2.abilities[1].ability.name :null,
+        ability3: pokeData2.abilities.length >2 ? pokeData2.abilities[2].ability.name :null,
+        base_experience: pokeData2.base_experience
+    }
+    Object.assign(pokemon[index], general);    
+}   
+
+async function renderOverlayStats(index) {
+    let response = await fetch(BASE_URL_SPECIES + (offset + index - offset + 1)); 
+    let pokeData = await response.json();
+    let response2 = await fetch(BASE_URL + (offset + index - offset + 1)); 
+    let pokeData2 = await response2.json();     
+    let stats = {
+        hp: pokeData2.stats[0].base_stat,
+        attack: pokeData2.stats[1].base_stat,
+        defense: pokeData2.stats[2].base_stat,
+        special_attack: pokeData2.stats[3].base_stat,
+        special_defense: pokeData2.stats[4].base_stat,
+        speed: pokeData2.stats[5].base_stat,
+    }
+    Object.assign(pokemon[index], stats);      
 }     
 
+async function renderStats(index){
+    let pokecard = document.getElementById("overlayStats");  
+    pokecard.innerHTML = "";  
+    pokecard.innerHTML = showOverlayStats(index);
+    setActiveTab("categorieStats");
+
+}
+
+async function renderGeneral(index){
+    let pokecard = document.getElementById("overlayStats");  
+    pokecard.innerHTML = "";  
+    pokecard.innerHTML = showOverlayGeneral(index);
+    setActiveTab("categorieGeneral");
+}
+
+function setActiveTab(activeId) {
+    document.querySelectorAll(".overlay-stats-categories-name").forEach(tab => tab.classList.remove("active-tab"));
+    document.getElementById(activeId).classList.add("active-tab");
+}
+
 function renderOverlay(index) {
-    let test = document.getElementById('overlay');
-    test.innerHTML = showOverlay(index);
-    test.classList.toggle('d-none');
+    let overlay = document.getElementById('overlay');
+    overlay.innerHTML = showOverlay(index);
+    overlay.classList.remove('d-none');
+    let pokecard = document.getElementById("overlayStats");  
+    pokecard.innerHTML = showOverlayGeneral(index);
 }
 
 function formatName(inputName) {
     let toFixed = inputName.charAt(0).toUpperCase() + inputName.slice(1);
     return toFixed;
+}
+
+function formatNumbers(inputNumber) {
+    let toFixed = inputNumber / 10;
+    let replace = toFixed.toString().replace(".",",");
+    return replace;
 }
 
 async function loadMore() {
@@ -100,16 +148,6 @@ function OverlaySpinnerOff() {
     }, 500);    
 }
 
-function renderFiltered(renderIndex) {
-    let statsRender = document.getElementById('overlayStats');
-    if (renderIndex == 1) {
-        statsRender = showOverlayGeneralStats();
-    }
-    if (renderIndex == 2) {
-        statsRender = showOverlayGeneralStats();
-    }
-}
-
 function noBubbling(event) {
     event.stopPropagation();
 }    
@@ -118,3 +156,30 @@ function toggleOverlay() {
     let overlayRef = document.getElementById('overlay')
     overlayRef.classList.toggle('d-none');  
 }
+
+async function next(currentIndex) {
+    currentIndex = Number(currentIndex);  
+    currentIndex = currentIndex + 1;
+    console.log(currentIndex);
+    
+    if (currentIndex + 1 > pokemon.length) {
+        try {
+            await loadMore();
+        } catch (error) {
+            console.log(error);            
+        }   
+        setTimeout(function() {
+            renderOverlay(currentIndex);;
+        }, 500)             
+    }
+    await renderOverlay(currentIndex);
+}
+
+async function previous(currentIndex) {
+    currentIndex = Number(currentIndex);  
+    if (currentIndex != 0) {
+        currentIndex = (currentIndex - 1) % pokemon.length;
+        await renderOverlay(currentIndex);
+    }
+}
+
--- script/template.js
+++ script/template.js
@@ -22,81 +22,76 @@ function showPokeCard(pokeData, index) {
 
 function showOverlay(index) {
     return `
-    <div class="overlay-content-box" onclick="noBubbling(event)">
-            <div class="overlay-number-name">
-                <span class="poke-number" id="pokeNumber">#${pokemon[index].id}</span>
-                <span class="poke-name" id="pokeName">${formatName(pokemon[index].name)}</span>
-                <div></div>
-            </div>
-            <div style="background-color:green" class="overlaypicture-box">
-                <img id="pokeImage" class="poke-image-overlay" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon[index].id}.svg" alt="">
-            </div>
-            <div id="" class="overlay-type-container">
-                                        <div class="${pokemon[index].type1}"></div>
-                        <div class="${pokemon[index].type2}"></div>             
-            </div>
-            <div class="overlay-stats-container">
-                <div class="overlay-stats-categories">
-                    <div class="overlay-stats-categories-name">
-                        <span onclick="renderFiltered(1)">Allgemein</span>
-                    </div>
-                    <div class="overlay-stats-categories-border"></div>
-                    <div class="overlay-stats-categories-name">
-                        <span onclick="renderFiltered(2)">Statistiken</span>
-                    </div>  
-                    <div class="overlay-stats-categories-border"></div>       
-                    <div class="overlay-stats-categories-name">
-                        <span>Evolution</span>
-                    </div>
+    	    <div class="overlay-content-box" onclick="noBubbling(event)">
+                <div class="overlay-number-name">
+                    <span class="poke-number" id="pokeNumber">#${pokemon[index].id}</span>
+                    <span class="poke-name" id="pokeName">${formatName(pokemon[index].name)}</span>
+                    <div></div>
                 </div>
-
-                <div id="overlayStats" class="overlay-stats">
-
-
+                <div style="background-color:${pokemon[index].backgroundcolor}" class="overlaypicture-box">
+                    <img id="pokeImage" class="poke-image-overlay" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon[index].id}.svg" alt="">
                 </div>
-
-
-
-
+                <div id="" class="overlay-type-container">
+                    <div class="${pokemon[index].type1}"></div>
+                    <div class="${pokemon[index].type2}"></div>             
+                </div>
+                <div id="overlayStatsContainer" class="overlay-stats-container">
+                    <div class="overlay-stats-categories">
+                        <div id="categorieGeneral" class="overlay-stats-categories-name active-tab">
+                            <span onclick="renderGeneral('${index}')">Allgemein</span>
+                        </div>
+                        <div class="overlay-stats-categories-border"></div>
+                        <div id="categorieStats" class="overlay-stats-categories-name">
+                            <span onclick="renderStats('${index}')">Statistiken</span>
+                        </div>  
+                        <div class="overlay-stats-categories-border"></div>       
+                        <div id="categorieEvolution" class="overlay-stats-categories-name">
+                            <span>Evolution</span>
+                        </div>
+                    </div>
+                <div id="overlayStats" class="overlay-stats"></div>
+                <div class="container-arrows-right-left">
+                    <img onclick="previous('${index}')" class="arrows-right-left" src="./assets/image/arrow-left.png" alt="">
+                    <img onclick="next('${index}')" class="arrows-right-left" src="./assets/image/arrow-right.png"" alt="">
+                </div>
             </div>
-    `
+             `
 }
 
-function showOverlayGeneralStats() {
+function showOverlayGeneral(index) {
     return `
             <div class="overlay-stats-general">
                 <table class="overlay-stats-general-table">
                     <tr>
                         <th class="overlay-stats-general-table-row">Größe</th>
-                        <th class="overlay-stats-general-table-row">: 0,7m</th>
+                        <th class="overlay-stats-general-table-row">: ${formatNumbers(pokemon[index].height)}m</th>
                     </tr>
                     <tr>
                         <th class="overlay-stats-general-table-row">Gewicht</th>
-                        <th class="overlay-stats-general-table-row">: 6,9kg</th>
+                        <th class="overlay-stats-general-table-row">: ${formatNumbers(pokemon[index].weight)}kg</th>
                     </tr>
                     <tr>
                         <th class="overlay-stats-general-table-row">Farbe</th>
-                        <th class="overlay-stats-general-table-row">: Grün</th>
+                        <th class="overlay-stats-general-table-row">: ${formatName(pokemon[index].backgroundcolor)}</th>
                     </tr>
                     <tr>
                         <th class="overlay-stats-general-table-row">Fähigkeiten</th>
-                        <th class="overlay-stats-general-table-row">: Notdünger, Chlorophyll</th>
+                        <th class="overlay-stats-general-table-row">: ${formatName(pokemon[index].ability1)}${pokemon[index].ability2 ? `, ${formatName(pokemon[index].ability2)}${pokemon[index].ability3 ? `, ${formatName(pokemon[index].ability3)}` : ""}` : ""}</th>
                     </tr>
                     <tr>
                         <th class="overlay-stats-general-table-row">Ei-Gruppe</th>
-                        <th class="overlay-stats-general-table-row">: Monster, Pflanzen</th>
+                        <th class="overlay-stats-general-table-row">: ${formatName(pokemon[index].egg1)}${pokemon[index].egg2 ? `, ${formatName(pokemon[index].egg2)}` : ""}</th>
                     </tr>
                     <tr>
-                        <th class="overlay-stats-general-table-row">EP bis Level 100</th>
-                        <th class="overlay-stats-general-table-row">: 1.059.860</th>
+                        <th class="overlay-stats-general-table-row">Basis Erfahrung</th>
+                        <th class="overlay-stats-general-table-row">: ${pokemon[index].base_experience}</th>
                     </tr>
                 </table>
             </div>  
-    `
-    
+    `    
 }
 
-function showOverlayGeneralStats() {
+function showOverlayStats(index) {
     return `
                         <div class="overlay-stats-stats">
                         <table class="overlay-stats-stats-table">
@@ -106,23 +101,23 @@ function showOverlayGeneralStats() {
                             </tr>
                             <tr>
                                 <th class="overlay-stats-general-table-row">Angriff</th>
-                                <th class="overlay-stats-general-table-row">: 49</th>
+                                <th class="overlay-stats-general-table-row">: ${pokemon[index].attack}</th>
                             </tr>
                             <tr>
                                 <th class="overlay-stats-general-table-row">Verteidigung</th>
-                                <th class="overlay-stats-general-table-row">: 49</th>
+                                <th class="overlay-stats-general-table-row">: ${pokemon[index].defense}</th>
                             </tr>
                             <tr>
                                 <th class="overlay-stats-general-table-row">Spezial-Attacke</th>
-                                <th class="overlay-stats-general-table-row">: 65</th>
+                                <th class="overlay-stats-general-table-row">: ${pokemon[index].special_attack}</th>
                             </tr>
                             <tr>
                                 <th class="overlay-stats-general-table-row">Spezial-Verteidigung</th>
-                                <th class="overlay-stats-general-table-row">: 65</th>
+                                <th class="overlay-stats-general-table-row">: ${pokemon[index].special_defense}</th>
                             </tr>
                             <tr>
                                 <th class="overlay-stats-general-table-row">Geschwindigkeit</th>
-                                <th class="overlay-stats-general-table-row">: 45</th>
+                                <th class="overlay-stats-general-table-row">: ${pokemon[index].speed}</th>
                             </tr>
                         </table>
                     </div>
